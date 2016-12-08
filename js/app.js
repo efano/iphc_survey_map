fb712ebabb928ff9b5ee31ed68fb9638158ebda6
efano <efa235@uky.edu>
2016-12-07 21:36:53 -0500

    regions infoWindow on mouseover

--- js/app.js
+++ js/app.js
@@ -27,14 +27,10 @@
     // load stations and regions
     omnivore.csv('data/iphc_stations.csv').on('ready', function (e) {
         $.getJSON('data/iphc_regions.json', function (data) {
-
             // load  outlines
             $.getJSON('data/iphc_outline.json', function (outlineData) {
-
                 drawMap(e.target.toGeoJSON(), data, outlineData)
-
             });
-
         });
     }).on('error', function (e) {
         console.log(e.error[0].message);
@@ -73,7 +69,6 @@ L.map('map', {
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // draw symbols for stations and regions
     function drawMap(sta, areas, outlinesData) {
-
         //
         /*if (layer.feature.properties['pct_bin_' + currentYear] == 1) {
                 layer.setStyle({
@@ -102,7 +97,7 @@ L.map('map', {
                 };
             }
         }).addTo(map).bringToBack();
-
+        //
         stations = L.geoJson(sta, {
             pointToLayer: function (feature, layer) {
                 return L.circleMarker(layer, {
@@ -112,57 +107,41 @@ L.map('map', {
                     , fillOpacity: 0
                     , radius: calcRadius(feature.properties.lbs_t_2015)
                 });
-            },
-            onEachFeature: function(feature, layer) {
-
-                layer.on('mouseover', function(e) {
-
+            }
+            , onEachFeature: function (feature, layer) {
+                layer.on('mouseover', function (e) {
                     layer.setStyle({
                         weight: 1
                         , color: '#f5f5f5'
                         , fillColor: '#f5f5f5'
-                        , fillOpacity: .3
+                        , fillOpacity: .1
                     });
-
-
                     var targetIphcArea = layer.feature.properties.iphc_area;
-
-                    regions.eachLayer(function(regionLayer) {
-                      if(targetIphcArea == regionLayer.feature.properties.REG_AREA){
-                        infoWindow(regionLayer.feature.properties);
-                        regionLayer.setStyle({
-                            fillOpacity: .6
-                        })
-                      }
-                    })
-
-                    $('#info').show();
-
-
+                    regions.eachLayer(function (regionLayer) {
+                        if (targetIphcArea == regionLayer.feature.properties.REG_AREA) {
+                            infoWindow(regionLayer.feature.properties);
+                            regionLayer.setStyle({
+                                fillOpacity: .1
+                            })
+                            $('#info').show();
+                        }
+                    });
                 });
-
-
-                layer.on('mouseout', function(e) {
-
+                layer.on('mouseout', function (e) {
                     layer.setStyle({
                         color: '#14AAC8'
                         , opacity: 1
                         , weight: 1
                         , fillOpacity: 0
                     });
-
                     regions.setStyle({
                         fillOpacity: 0
                     })
-
                     $('#info').hide();
-
                 });
-
-
             }
         }).addTo(map);
-
+        //
         outlines = L.geoJson(outlinesData, {
             style: function (feature) {
                 return {
@@ -175,12 +154,38 @@ L.map('map', {
                 };
             }
         }).addTo(map).bringToFront();
-
+        //
         sequenceUI();
         updateSymbols();
+        infoWindow();
+        //
+        /*regions.on('mouseover', function (e) {
+            var props = e.layer.feature.properties;
+            regions.eachLayer(function (layer) {
+                if (props.REG_AREA == layer.feature.properties.REG_AREA) {
+                    layer.setStyle({
+                        weight: 0
+                        , color: '#f5f5f5'
+                        , fillColor: '#f5f5f5'
+                        , fillOpacity: .07
+                    });
+                }
+            })
+            $('#info').show();
+        });
+        regions.on('mouseout', function (layer) {
+            regions.eachLayer(function (layer) {
+                layer.setStyle({
+                    color: '#f5f5f5'
+                    , weight: 0
+                    , fillOpacity: 0
+                    , fillColor: '#f5f5f5'
+                });
+            })
+            $('#info').hide();
+        });*/
     }
     //
-
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // resizes symbols
     function updateSymbols() {
@@ -214,14 +219,26 @@ L.map('map', {
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // infowindow
     function infoWindow(props) {
-
-        $('#info span').text(props.REG_AREA);
-        $(".info_year span").text(currentYear);
-        $(".info_T_CNT span").text(props['TCNT_' + String(currentYear)].toLocaleString());
-        $(".info_T_LBS span").text(props['TLBS_' + String(currentYear)].toLocaleString());
-        $(".info_PCT_T span").text(props['PTO32_' + String(currentYear)].toLocaleString());
-        $(".info_T_LBS_O span").text(props['LO32_' + String(currentYear)].toLocaleString());
-
+        var info = $('#info');
+        regions.on('mouseover', function (e) {
+            info.show();
+            var props = e.layer.feature.properties;
+            $('#info span').text(props.REG_AREA);
+            $(".info_year span").text(currentYear);
+            $(".info_T_CNT span").text(props['TCNT_' + String(currentYear)].toLocaleString());
+            $(".info_T_LBS span").text(props['TLBS_' + String(currentYear)].toLocaleString());
+            $(".info_PCT_T span").text(props['PTO32_' + String(currentYear)].toLocaleString());
+            $(".info_T_LBS_O span").text(props['LO32_' + String(currentYear)].toLocaleString());
+            e.layer.setStyle({
+                fillOpacity: .1
+            });
+        });
+        regions.on('mouseout', function (e) {
+            info.hide();
+            e.layer.setStyle({
+                fillOpacity: 0
+            });
+        });
     };
 })
 ();
